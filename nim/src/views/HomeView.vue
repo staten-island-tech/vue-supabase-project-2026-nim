d0ec2b64af2c18c691dd501dafde64a0f5b0d215
ip208 <igorp4@nycstudents.net>
2026-05-29 13:42:42 -0400

    r

--- nim/src/views/HomeView.vue
+++ nim/src/views/HomeView.vue
@@ -1,5 +1,11 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+
+export const supabase = createClient(supabaseUrl, supabaseKey)
 </script>
 
 <template>
